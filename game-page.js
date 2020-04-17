4835467d09654b2afbac8e22e2625596352adcd7
Philip Kouchner <philipk179@gmail.com>
2020-04-16 22:44:43 -0700

    working on js file

--- game-page.js
+++ game-page.js
@@ -1,4 +1,71 @@
+// var hostanmeUrl = window.location.href;
 
+var Questions = [
+
+    {
+        answer: ["a", "b", "c", "d"],
+        question: "How are you tommoroy?",
+        correctAnswer: 2
+    },
+    {
+        answer: ["a", "b", "c", "d"],
+        question: "How are you yesterday?",
+        correctAnswer: 1
+    },
+
+
+    {
+        answer: ["a", "b", "c", "d"],
+        questio: "How are you today?",
+        correctAnswer: 3
+    }
+
+];
+
+var question = document.querySelector("#question");
+var firstOption = document.querySelector("#option1");
+var secondOption = document.querySelector("#option2");
+var thirdOption = document.querySelector("#option3");
+var lastOption = document.querySelector("#option4");
+
+var containerForm = document.querySelector(".container");
+var items = document.querySelector("#items");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var timerRun = document.querySelector("#timer-run");
+var gameSeconds = 10;
+
+
+
+
+var gameToPlay = setInterval(function () {
+
+    gameSeconds--;
+    timerRun.textContent = "Time: " + gameSeconds;
+
+    if (gameSeconds === 0) {
+
+        clearInterval(gameToPlay);
+        containerForm.setAttribute("class", "container-display");
+        question.style.display = "none";
+        items.style.display = "none";
+
+    }
+
+}, 1000)
 
 
 
