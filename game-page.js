5f61c459c4e15af42c003ebc1f7891719beb6bba
Philip Kouchner <philipk179@gmail.com>
2020-04-22 10:26:13 -0700

    finish local storage

--- game-page.js
+++ game-page.js
@@ -42,294 +42,295 @@ var Questions = [
     }
 ];
 
-window.onload = function () {
-
+// window.onload = function () {
+setArray();
+function setArray() {
     arrayHistoryGame = JSON.parse(localStorage.getItem("names")); //get data from storage
     console.log(arrayHistoryGame);
     if (arrayHistoryGame == null) {
         arrayHistoryGame = []; //if data exist
         console.log(arrayHistoryGame);
-        // for (var i = 0; i < arrayHistoryGame.length; i++) {
-
-        //     var labelTag = document.createElement("li");
-        //     labelTag.textContent = arrayHistoryGame[i].name + " " + arrayHistoryGame[i].score;
-        //     labelTag.setAttribute("data-id", i);
-        //     register.appendChild(labelTag);
-        // }
-
-
-        // } else { //if nothing exist in storage
-
-        // }
     }
-
-    btnStartGame.addEventListener("click", function () {
-
-        startGameQuiz();
-    });
-
-    btnHighIdScore.addEventListener("click", function () {
-
-        flagBtnPress = true;
-        checkStyleElements(flagBtnPress);
-        containerScore.classList.remove("hide");
-
-        if (window.localStorage.getItem("names")) {
-
-
-            generateElement();
-
-        }
-    });
-
-
-    function checkStyleElements(flagBtnPress) {
-
-        var z = document.querySelector(".title").getAttribute("class");
-        var x = containerScore.getAttribute("class");
-        var y = document.querySelector(".nav-bar").getAttribute("class");
-
-        if (flagBtnPress) {
-
-            console.log(flagBtnPress);
-            if (z === "title") {
-
-                document.querySelector(".title").classList.add("hide");
-
-            }
-
-            if (y === "nav-bar") {
-
-                document.querySelector(".nav-bar").classList.add("hide");
-            }
-
-        } else {
-
-
-            document.querySelector(".title").classList.remove("hide");
-            document.querySelector(".nav-bar").classList.remove("hide");
-            containerForm.classList.add("hide");
-            containerScore.classList.add("hide");
-        }
-
-
-    }
-
-    btnHomePage.addEventListener("click", function () {
-        flagBtnPress = false;
-        checkStyleElements(flagBtnPress);
-        btnHighIdScore.disabled = false;
-    });
-
-    function startGameQuiz() {
-
-        document.querySelector(".title").classList.add("hide");
-        if (question.className === "askQuestion hide") {
-            question.classList.remove("hide");
-            if (items.className === "items hide") {
-                items.classList.remove("hide");
-            }
-        }
-        document.querySelector("ul").classList.add("list-group");
-        question.classList.add("list-group-item");
-        option1.classList.add("list-group-item");
-        option2.classList.add("list-group-item");
-        option3.classList.add("list-group-item");
-        option4.classList.add("list-group-item");
-        option1.classList.add("list-group-item");
-        option1.textContent = "";
-        option2.textContent = "";
-        option3.textContent = "";
-        option4.textContent = "";
-        question.textContent = "";
-        horizon.classList.add("hide");
-        paragraph.textContent = "";
-        userScore = 0;
-
-        btnHighIdScore.disabled = true;
-        renderID();
-        startIntervel();
-
-    }
-    function renderID() {
-
-        changeTheHTML(numQuestion);
-        var newId = 0;
-        var checkI = 0;
-        arr = [option1, option2, option3, option4];
-
-        for (var i = 0; i < 4; i++) {
-
-            arr[i].setAttribute("data-id", i);
-            arr[i].addEventListener("click", temp, true);
-        }
-    }
-
-    var temp = function () {
-
-        checkI = this.getAttribute("data-id");
-        newId = parseInt(checkI);
-
-
-        horizon.classList.remove("hide");
-        if (numQuestion < Questions.length) {
-
-            console.log(newId);
-
-            if (Questions[numQuestion].correctAnswer === newId) {
-
-                paragraph.textContent = "Correct";
-                userScore += 10;
-            } else {
-
-                gameSeconds -= 5;
-                paragraph.textContent = "Wrong";
-            }
-            numQuestion++;
-            changeTheHTML(numQuestion);
-
-        }
-
-    }
-
-    function changeTheHTML(numQuestion) {
-
-
-        setTimeout(function () {
-
-
-            if (numQuestion <= Questions.length - 1) {
-
-                question.textContent = Questions[numQuestion].question;
-                option1.textContent = Questions[numQuestion].answers[0];
-                option2.textContent = Questions[numQuestion].answers[1];
-                option3.textContent = Questions[numQuestion].answers[2];
-                option4.textContent = Questions[numQuestion].answers[3];
-                horizon.classList.add("hide");
-                paragraph.textContent = "";
-
-            }
-
-        }, 500);
-
-    }
-
-    function startIntervel() {
-
-
-        var gameToPlay = setInterval(function () {
-
-            gameSeconds--;
-            timerRun.textContent = "Time: " + gameSeconds;
-
-            if (gameSeconds <= 0 || numQuestion > Questions.length - 1) {
-
-                clearInterval(gameToPlay);
-                scoreDisplya.textContent = "Your final score is " + userScore;
-                timerRun.textContent = "Timer";
-                classChange();
-            }
-
-        }, 1000)
-    }
-
-
-    function classChange() {
-
-        if (containerForm.className === "container-form hide") {
-
-            containerForm.classList.remove("hide");
-        }
-        question.classList.add("hide");
-        question.classList.remove("list-group-item");
-        items.classList.add("hide");
-        numQuestion = 0;
-        gameSeconds = 60;
-    }
-
-    btnSubmit.addEventListener("click", function (event) {
-
-        event.preventDefault();
-        var input;
-        checkInput();
-    });
-
-    function checkInput() {
-
-        input = textInput.value;
-
-        if (input === "" || !isNaN(input)) {
-            console.log(input);
-            alert("Wrong input, Try again");
-            textInput.value = "";
-
-        } else {
-
-
-            showHighScore();
-            renderPlayerNames(input, userScore);
-        }
-    }
-
-    function showHighScore() {
-
-        if (containerForm.className === "container-form") {
-            containerForm.classList.add("hide");
-        } else {
-
-            containerForm.classList.add("hide");
-        }
-        if (containerScore.className === "container-score hide") {
-            containerScore.classList.remove("hide");
-        } else {
-            containerScore.classList.add("hide");
-        }
-
-        var nav = document.querySelector(".nav-bar");
-
-        if (nav.className === "nav-bar") {
-            nav.classList.add("hide");
-        } else {
-            nav.classList.remove("hide");
-        }
-
-    }
-
-    function renderPlayerNames(input, userScore) {
-
-        var Player = {
-
-            name: input.trim(),
-            score: userScore
-        };
-
-        arrayHistoryGame.push(Player);
-        textInput.value = "";
-        window.localStorage.setItem("names", JSON.stringify(arrayHistoryGame));
-        generateElement();
-    }
-
-    btnClearHistory.addEventListener("click", function () {
-
-        window.localStorage.removeItem("names");
-        arrayHistoryGame = [];
-        while (register.firstChild) {
-            register.removeChild(register.firstChild);
-        }
-    });
 }
+// arrayHistoryGame = JSON.parse(localStorage.getItem("names")); //get data from storage
+// console.log(arrayHistoryGame);
+// if (arrayHistoryGame == null) {
+//     arrayHistoryGame = []; //if data exist
+//     console.log(arrayHistoryGame);
+// for (var i = 0; i < arrayHistoryGame.length; i++) {
+
+//     var labelTag = document.createElement("li");
+//     labelTag.textContent = arrayHistoryGame[i].name + " " + arrayHistoryGame[i].score;
+//     labelTag.setAttribute("data-id", i);
+//     register.appendChild(labelTag);
+// }
+
+
+// } else { //if nothing exist in storage
+
+// }
+// }
+
+btnStartGame.addEventListener("click", function () {
+
+    startGameQuiz();
+});
+
+btnHighIdScore.addEventListener("click", function () {
+
+    flagBtnPress = true;
+    checkStyleElements(flagBtnPress);
+    containerScore.classList.remove("hide");
+
+    if (window.localStorage.getItem("names")) {
+
+
+        generateElement();
+
+    }
+});
+
+
+function checkStyleElements(flagBtnPress) {
+
+    var z = document.querySelector(".title").getAttribute("class");
+    var x = containerScore.getAttribute("class");
+    var y = document.querySelector(".nav-bar").getAttribute("class");
+
+    if (flagBtnPress) {
+
+        console.log(flagBtnPress);
+        if (z === "title") {
+
+            document.querySelector(".title").classList.add("hide");
+
+        }
+
+        if (y === "nav-bar") {
+
+            document.querySelector(".nav-bar").classList.add("hide");
+        }
+
+    } else {
+
+
+        document.querySelector(".title").classList.remove("hide");
+        document.querySelector(".nav-bar").classList.remove("hide");
+        containerForm.classList.add("hide");
+        containerScore.classList.add("hide");
+    }
+
+
+}
+
+btnHomePage.addEventListener("click", function () {
+    flagBtnPress = false;
+    checkStyleElements(flagBtnPress);
+    btnHighIdScore.disabled = false;
+});
+
+function startGameQuiz() {
+
+    document.querySelector(".title").classList.add("hide");
+    if (question.className === "askQuestion hide") {
+        question.classList.remove("hide");
+        if (items.className === "items hide") {
+            items.classList.remove("hide");
+        }
+    }
+    document.querySelector("ul").classList.add("list-group");
+    question.classList.add("list-group-item");
+    option1.classList.add("list-group-item");
+    option2.classList.add("list-group-item");
+    option3.classList.add("list-group-item");
+    option4.classList.add("list-group-item");
+    option1.classList.add("list-group-item");
+    option1.textContent = "";
+    option2.textContent = "";
+    option3.textContent = "";
+    option4.textContent = "";
+    question.textContent = "";
+    horizon.classList.add("hide");
+    paragraph.textContent = "";
+    userScore = 0;
+
+    btnHighIdScore.disabled = true;
+    renderID();
+    startIntervel();
+
+}
+function renderID() {
+
+    changeTheHTML(numQuestion);
+    var newId = 0;
+    var checkI = 0;
+    arr = [option1, option2, option3, option4];
+
+    for (var i = 0; i < 4; i++) {
+
+        arr[i].setAttribute("data-id", i);
+        arr[i].addEventListener("click", temp, true);
+    }
+}
+
+var temp = function () {
+
+    checkI = this.getAttribute("data-id");
+    newId = parseInt(checkI);
+
+
+    horizon.classList.remove("hide");
+    if (numQuestion < Questions.length) {
+
+        console.log(newId);
+
+        if (Questions[numQuestion].correctAnswer === newId) {
+
+            paragraph.textContent = "Correct";
+            userScore += 10;
+        } else {
+
+            gameSeconds -= 5;
+            paragraph.textContent = "Wrong";
+        }
+        numQuestion++;
+        changeTheHTML(numQuestion);
+
+    }
+
+}
+
+function changeTheHTML(numQuestion) {
+
+
+    setTimeout(function () {
+
+
+        if (numQuestion <= Questions.length - 1) {
+
+            question.textContent = Questions[numQuestion].question;
+            option1.textContent = Questions[numQuestion].answers[0];
+            option2.textContent = Questions[numQuestion].answers[1];
+            option3.textContent = Questions[numQuestion].answers[2];
+            option4.textContent = Questions[numQuestion].answers[3];
+            horizon.classList.add("hide");
+            paragraph.textContent = "";
+
+        }
+
+    }, 500);
+
+}
+
+function startIntervel() {
+
+
+    var gameToPlay = setInterval(function () {
+
+        gameSeconds--;
+        timerRun.textContent = "Time: " + gameSeconds;
+
+        if (gameSeconds <= 0 || numQuestion > Questions.length - 1) {
+
+            clearInterval(gameToPlay);
+            scoreDisplya.textContent = "Your final score is " + userScore;
+            timerRun.textContent = "Timer";
+            classChange();
+        }
+
+    }, 1000)
+}
+
+
+function classChange() {
+
+    if (containerForm.className === "container-form hide") {
+
+        containerForm.classList.remove("hide");
+    }
+    question.classList.add("hide");
+    question.classList.remove("list-group-item");
+    items.classList.add("hide");
+    numQuestion = 0;
+    gameSeconds = 60;
+}
+
+btnSubmit.addEventListener("click", function (event) {
+
+    event.preventDefault();
+    var input;
+    checkInput();
+});
+
+function checkInput() {
+
+    input = textInput.value;
+
+    if (input === "" || !isNaN(input)) {
+        console.log(input);
+        alert("Wrong input, Try again");
+        textInput.value = "";
+
+    } else {
+
+
+        showHighScore();
+        renderPlayerNames(input, userScore);
+    }
+}
+
+function showHighScore() {
+
+    if (containerForm.className === "container-form") {
+        containerForm.classList.add("hide");
+    } else {
+
+        containerForm.classList.add("hide");
+    }
+    if (containerScore.className === "container-score hide") {
+        containerScore.classList.remove("hide");
+    } else {
+        containerScore.classList.add("hide");
+    }
+
+    var nav = document.querySelector(".nav-bar");
+
+    if (nav.className === "nav-bar") {
+        nav.classList.add("hide");
+    } else {
+        nav.classList.remove("hide");
+    }
+
+}
+
+function renderPlayerNames(input, userScore) {
+
+    var Player = {
+
+        name: input.trim(),
+        score: userScore
+    };
+
+    arrayHistoryGame.push(Player);
+    textInput.value = "";
+    window.localStorage.setItem("names", JSON.stringify(arrayHistoryGame));
+    generateElement();
+}
+
+btnClearHistory.addEventListener("click", function () {
+
+    window.localStorage.removeItem("names");
+    arrayHistoryGame = [];
+    while (register.firstChild) {
+        register.removeChild(register.firstChild);
+    }
+});
 
 function generateElement() {
 
     register.innerHTML = "";
     arrayNames = JSON.parse(window.localStorage.getItem("names"));
-    var pq = arrayNames.sort(function () {
 
-        if (arrayNames[0].score < arrayNames[1].score) {
-            return -1;
-        }
-    });
-    console.log(pq);
     for (var i = 0; i < arrayNames.length; i++) {
 
         var labelTag = document.createElement("li");
